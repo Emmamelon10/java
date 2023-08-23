72e92b7d8e2ee0eb73987730667725c46fe49b7d
Emmamelon10 <131699411+Emmamelon10@users.noreply.github.com>
2023-08-23 09:54:21 -0300

    Add files via upload

---
+++ deSanguinesa/src/deSanguinesa/CadenasNombre.java
@@ -0,0 +1,18 @@
+package deSanguinesa;
+import java.util.Scanner;
+
+
+public class CadenasNombre {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String name;
+		
+		System.out.print("Ingrese su numbre porfavor: ");
+		name = scan.nextLine();
+		System.out.println(name);
+
+	}
+
+}
